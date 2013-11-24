9a78ed161740fa9168a77d3422a8a58a74b75284
tubedude <rtrevisan@gmail.com>
2013-11-23 22:44:27 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XeroUploader2::Application.config.secret_key_base = '3a03607aaf036fa33f1ab6561a2c4eb6ebcf046b2ef25b7487406ab09d7b404ba74800ebb6252f879a31c537b71989e866cb6c37ce5f06bb40225f4816fb4c6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+XeroUploader2::Application.config.session_store :cookie_store, key: '_xero_uploader2_session'
