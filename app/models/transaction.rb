333ddafb1a3bf0921795ef320632b1f5b1f8a5c1
tubedude <rtrevisan@gmail.com>
2014-09-30 12:10:58 -0300

    Added validation to Number

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :fund
 
   validates :fund, presence: true
+  validates :unit_amount, :numericality => { :greater_than_or_equal_to => 0 }
 
   def self.accessible_attributes
     [ "xero_type", "name", "date", "due_date", "description", "quantity", "unit_amount", "account_code", "currency_code", "currency_rate", "reference", "invoice_number"]
